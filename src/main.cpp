6d4523605c19049554b49196d6258023f7f90634
Nolan Lum <nolan@nolm.name>
2014-06-19 23:23:55 -0700

    We now build on OSX

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
-#ifdef _HAVE_WINDOWS_H
-#include <windows.h>
-#endif
-
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glut.h>
 #include <iostream>
 
 #include <pugixml.hpp>
 
 #include <respack.h>
+
+#ifdef _HAVE_WINDOWS_H
+#include <windows.h>
+#endif
+
+#if __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
 
 int main() {
   ResourcePack pack("../../respacks/Default/");
